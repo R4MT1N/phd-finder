537bda9f1a6587c51adedb82be7562a18cee718e
Ramtin B <bagheri@bitbaan.com>
2024-02-09 00:33:19 +0330

    fixed a problem with twente university positions

--- universities/netherlands/twente.py
+++ universities/netherlands/twente.py
@@ -21,6 +21,12 @@ class Twente(University):
         jobs = self._extract_jobs()
 
         for job in jobs:
+            category = clean_text(job.select_one('span.vacancies__results__vacancy__tags span[data-filter-name=category]').text)
+            faculty = clean_text(job.select_one('span.vacancies__results__vacancy__tags span[data-filter-name=faculty]').text)
+
+            if category != 'PhD' or faculty != 'EEMCS':
+                continue
+
             title = clean_text(job.select_one('b.vacancies__results__vacancy__title').text)
             link = job.select_one('a.vacancies__results__vacancy').attrs['href']
             end_at = read_date(clean_text(job.select('span.vacancies__results__vacancy__date')[1].find(text=True, recursive=False)), '%d %b %Y')
